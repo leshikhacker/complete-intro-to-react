642681240b45f5a89b797035afa5e4d22460e8af
leshikhacker <leshikhacker@gmail.com>
2018-03-10 03:45:00 +0200

    rewrite App on JSX, add webpack dev server

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,48 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-const MyTitle = function(props) {
-  return ce(
-    'div',
-    null,
-    ce(
-      'h1',
-      {
-        style: {
-          color: props.colour
-        }
-      },
-      null,
-      props.title
-    )
-  );
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    {
-      id: 'my-first-component'
-    },
-    ce(MyTitle, {
-      title: 'Lost',
-      colour: 'YellowGreen'
-    }),
-    ce(MyTitle, {
-      title: 'Gannibal',
-      colour: 'GreenYellow'
-    }),
-    ce(MyTitle, {
-      title: 'Supernatural',
-      colour: 'LimeGreen'
-    }),
-    ce(MyTitle, {
-      title: 'Teenwolf',
-      colour: 'Peru'
-    })
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
